d956d529616bc9381b7e28cad2ff48152c93e422
José Quintinno <credentium@outlook.com.br>
2025-11-28 12:16:36 -0300

    feature: Modelar a tabela da funcionalidade de Base de Dados

--- credentium.sql
+++ credentium.sql
@@ -1,5 +1,6 @@
 drop table if exists tb_plataforma cascade;
 drop table if exists tb_credencial cascade;
+drop table if exists tb_base_dados cascade;
 
 create table if not exists tb_plataforma (
   code serial not null,
@@ -30,5 +31,22 @@ create table if not exists tb_credencial (
   constraint un_credencial unique (descricao, usuario, senha)
 );
 
+create table if not exists tb_base_dados (
+  code serial not null,
+  descricao varchar(255) not null,
+  nome varchar(255) not null,
+  senha varchar(255) not null,
+  host varchar(255) not null,
+  porta varchar(255) null,
+  url varchar(255) null,
+  created_at timestamp not null default now(),
+  updated_at timestamp not null default now(),
+  deleted_at timestamp null,
+  active boolean not null default true,
+  constraint pk_base_dados primary key (code),
+  constraint un_base_dados unique (descricao, nome, host)
+);
+
 -- select * from tb_plataforma;
 -- select * from tb_credencial;
+-- select * from tb_base_dados;
